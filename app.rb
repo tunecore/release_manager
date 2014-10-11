d462f49c06e0673f0fd186c8a285826e146adbfa
Troy Denkinger <troy@tunecore.com>
2014-10-11 13:30:34 -0400

    Testing

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require "json"
 
 class ReleaseManager < Sinatra::Application
-  before do
-    request.body.rewind
-    @request_payload = JSON.parse request.body.read
-  end
-
   get "/" do
     erb :index
   end
 
   post "/story" do
+    request.body.rewind
+    @request_payload = JSON.parse request.body.read
     p @request_payload
   end
 end
