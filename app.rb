018e63b3c06b10467e138d5dfb2083bda343c6a9
Troy Denkinger <troy@tunecore.com>
2014-10-11 13:27:59 -0400

    Parse the body

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
+require "json"
+
 class ReleaseManager < Sinatra::Application
+  before do
+    request.body.rewind
+    @request_payload = JSON.parse request.body.read
+  end
+
   get "/" do
     erb :index
   end
 
   post "/story" do
-    story = params[:story]
-    p story
+    p @request_payload
   end
 end
