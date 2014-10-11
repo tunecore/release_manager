658245a3189387c6daea4a0f59afe6066e735f42
Troy Denkinger <troy@tunecore.com>
2014-10-11 15:47:12 -0400

    Add callback to create story.

--- app.rb
+++ app.rb
@@ -17,9 +17,7 @@ class ReleaseManager < Sinatra::Application
     payload = JSON.parse request.body.read
 
     if payload["changes"][0]["new_values"]["current_state"] == "accepted"
-      p payload
       status = @conn.post "/services/v5/projects/1185380/stories", story_details(payload)
-      p status
     end
   end
 
@@ -28,7 +26,7 @@ class ReleaseManager < Sinatra::Application
     { name: p["primary_resources"][0]["name"],
       description: p["primary_resources"][0]["url"],
       story_type: p["primary_resources"][0]["story_type"],
-      current_state: "deliered",
+      current_state: "delivered",
       estimate: 0
     })
   end
