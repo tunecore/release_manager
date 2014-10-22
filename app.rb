b590320ca13fe0f9d30635b0d4932643ed870e9d
Troy Denkinger <troy@tunecore.com>
2014-10-22 12:56:20 -0400

    Debugging

--- app.rb
+++ app.rb
@@ -17,7 +17,6 @@ class ReleaseManager < Sinatra::Application
     payload = JSON.parse request.body.read
 
     if payload["changes"][0]["new_values"]["current_state"] == "accepted"
-p payload
       status = @conn.post "/services/v5/projects/1185380/stories", story_details(payload)
     end
   end
@@ -31,7 +30,7 @@ p payload
       labels: []
     }
 
-    p["changes"][0]["new_values"]["labels"].each{ | label | story_details[:labels] << {"name" => label} }
+    # p["changes"][0]["new_values"]["labels"].each{ | label | story_details[:labels] << {"name" => label} }
 
     story_details[:estimate] = 0 if story_details[:story_type] == "feature"
 
